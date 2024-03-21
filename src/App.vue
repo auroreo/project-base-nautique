cdb534f5d17b08efb3bac9a6163038de4fd0f55a
Aurore VERNET <aurore.vernet@live.com>
2024-03-21 13:47:12 +0100

    navbar (errors because no template in pages)

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,12 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/individual">Individuel</RouterLink>
+        <RouterLink to="/group">Groupe</RouterLink>
+        <RouterLink to="/family">Famille</RouterLink>
       </nav>
-    </div>
   </header>
 
   <RouterView />

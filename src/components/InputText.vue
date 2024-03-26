ffbf9147af01d6e38c502fc62c5c4d77958c1d11
Aurore VERNET <aurore.vernet@live.com>
2024-03-26 08:42:02 +0100

    color red required

--- src/components/InputText.vue
+++ src/components/InputText.vue
@@ -5,8 +5,10 @@ const props = defineProps(['input'])
 </script>
 
 <template>
-
-<label :for="props.input.name" class="label-text"> {{ props.input.label }} </label>
+<div>
+    <label :for="props.input.name" class="label-text"> {{ props.input.label }} </label>
+    <span class="red-required" v-if="props.input.label === 'Nom' || props.input.label === 'Prénom' || props.input.label === 'Nom du group'" >*</span>
+</div>
 <input :type="props.input.type" :name="props.input.name" :id="props.input.id" class="input-text">
 
 </template>

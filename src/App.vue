3ced6dab01d0632539c4f30649eaacf4c34d3579
Nicolas-Personne <personne.nico@gmail.com>
2024-03-28 12:33:00 +0100

    fix updated

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,9 @@ import { onMounted, watch, ref } from 'vue'
 import { RouterView, useRouter } from 'vue-router'
 let timer = 300000 /*5min*/
 const router = useRouter()
+let timeout
 const resetTimer = () => {
-  setTimeout(() => {
+  timeout = setTimeout(() => {
     router.replace('/')
   }, timer)
 }
@@ -13,27 +14,28 @@ const secondTouch = ref(false)
 onMounted(() => {
   document.addEventListener('click', () => {
     timer = 300000
+    clearTimeout(timeout)
     resetTimer()
   })
 })
 
 window.onpointerdown = (e) => {
-  if (e.clientX < 100 && e.clientY < 100) {
+  if (e.clientX < 200 && e.clientY < 200) {
     firstTouch.value = true
   }
 }
 window.onpointerup = (e) => {
-  if (e.clientX < 100 && e.clientY < 100) {
+  if (e.clientX < 200 && e.clientY < 200) {
     firstTouch.value = false
   }
 }
 document.onpointerdown = (e) => {
-  if (e.clientX > window.screen.width - 100 && e.clientY > window.screen.height - 100) {
+  if (e.clientX > window.screen.width - 200 && e.clientY > window.screen.height - 200) {
     secondTouch.value = true
   }
 }
 document.onpointerup = (e) => {
-  if (e.clientX > window.screen.width - 100 && e.clientY > window.screen.height - 100) {
+  if (e.clientX > window.screen.width - 200 && e.clientY > window.screen.height - 200) {
     secondTouch.value = false
   }
 }
@@ -46,6 +48,8 @@ watch([firstTouch, secondTouch], ([newA, newB], [prevA, prevB]) => {
         router.replace('dashboard')
       }
     }, 1000)
+  } else {
+    count = 0
   }
 })
 </script>
